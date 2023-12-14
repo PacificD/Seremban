24ea25a7d68e4431725a2150455f8ef3d04838b5
PacificD <pacificd148@gmail.com>
2023-12-14 18:50:29 +0800

    docs: comment documentations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use seremban::add_one;
+
 mod convert_temperature;
 mod fibonacci;
 mod guessing_game;
@@ -6,10 +8,12 @@ mod string;
 fn main() {
     // converted_temperature::convert();
     // guessing_game::start();
-    let username_result = string::read_username_from_file();
-
-    match username_result {
-        Ok(username) => println!("username: {}", username),
-        Err(error) => println!("error: {}", error),
-    }
+    // let username_result = string::read_username_from_file();
+    //
+    // match username_result {
+    //     Ok(username) => println!("username: {}", username),
+    //     Err(error) => println!("error: {}", error),
+    // }
+    let res = add_one(4);
+    println!("res: {res}");
 }
