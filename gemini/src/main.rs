7d3be974f4d6e7dbfaaf909f30ce798c44739ece
PacificD <pacificd148@gmail.com>
2024-02-19 16:43:17 +0800

    feat(gemini): without input

--- gemini/src/main.rs
+++ gemini/src/main.rs
@@ -1,4 +1,4 @@
-use dotenv::dotenv;
+use dotenv::from_path;
 use reqwest::{Client, Error};
 use serde_json::{from_str, json, to_string, Value};
 use std::{env, io};
@@ -30,23 +30,27 @@ async fn post_gemini(gemini_target: String, prompt: String) -> Result<(), Error>
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    dotenv().ok();
-    let token = env::var("TOKEN").unwrap_or_else(|_| "token not found in .env file".to_string());
-    let proxy =
-        env::var("PROXY").unwrap_or_else(|_| "proxy string not found in .env file".to_string());
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 4 {
+        println!("Parameter error!");
+    }
+
     let gemini_target = format!(
         "{}/v1beta/models/gemini-pro:generateContent?key={}",
-        proxy, token
+        &args[1], // token,
+        &args[2]  // proxy
+                  // &args[3] prompt
     );
     // Get the user input
-    let mut user_input = String::new();
-    println!("Enter the prompt:");
-    io::stdin().read_line(&mut user_input);
+    // let mut user_input = String::new();
+    // println!("Enter the prompt: {gemini_target}");
+    // io::stdin().read_line(&mut user_input);
 
     let json_string = r#"{"contents":[{"parts":[{"text":""}]}]}"#;
     // Parse the JSON string into a serde_json::Value
     let mut value: Value = from_str(json_string).unwrap();
-    value["contents"][0]["parts"][0]["text"] = json!(user_input);
+    value["contents"][0]["parts"][0]["text"] = json!(&args[3]);
     // Convert the modified value back to a JSON string
     let prompt = to_string(&value).unwrap();
 
